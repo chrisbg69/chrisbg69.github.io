6a8ade52d6be3804391cdc470695cc78f8699dec
chrisbg69 <archangelbg@abv.bg>
2019-08-01 19:20:26 +0300

    Add slider animation on home page

---
+++ _site/assets/js/app.js
@@ -0,0 +1,11 @@
+// Initialise FlexSlider for Carousels
+$(window).load(function() {
+    $('.flexslider').flexslider({
+    animation: "fade",
+    itemHeight: 200,
+    directionNav: true,
+    slideshowSpeed: 5000,
+    animationSpeed: 600,
+    touch: true
+    });
+});
