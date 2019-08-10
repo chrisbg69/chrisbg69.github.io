66ad3d43f803b97323d8904ec2827881543aaceb
chrisbg <archangelbg@abv.bg>
2019-08-11 02:11:36 +0300

    Added gulp and Sass

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+"use strict";
+
+let gulp = require("gulp"),
+	autoprefixer = require("gulp-autoprefixer"),
+	exec = require("gulp-exec"),
+	browserSync = require('browser-sync').create(),
+	sass = require('gulp-sass'),
+	cp = require("child_process");
+gulp
+gulp.task("css", function() {
+	return gulp.src( 'assets/scss/**/*.scss' )
+		.pipe( sass().on('error', sass.logError) )
+		.pipe( autoprefixer() )
+		.pipe( gulp.dest( './_site/css/' ) )
+		.pipe( browserSync.stream({ match: '**/*.css' }) )
+	;
+});
+
+// Jekyll
+gulp.task("jekylldev", function() {
+	return cp.spawn("bundle", ["exec", "jekyll", "build"], { stdio: "inherit", shell: true });
+});
+
+// Jekyll
+gulp.task("jekyllprod", function() {
+	return cp.spawn("bundle", ["exec", "jekyll", "build --baseurl /"], { stdio: "inherit", shell: true });
+});
+
+gulp.task("watch", function() {
+
+	browserSync.init({
+		server: {
+            baseDir: "./_site/"
+		}
+	});
+
+	gulp.watch( 'assets/scss/**/*.scss', gulp.series('css') );
+
+	gulp.watch(
+		[
+			"./*.html",
+			"./_includes/*.html",
+			"./_layouts/*.html",
+			"./_posts/**/*.*"
+		]
+	).on('change', gulp.series('jekylldev', 'css') );
+
+	gulp.watch( '_site/**/*.html' ).on('change', browserSync.reload );
+	gulp.watch( '_site/**/*.js' ).on('change', browserSync.reload );
+});
+
+gulp.task("deploy", gulp.series('jekyllprod', 'css'));
+
+gulp.task("default", gulp.series('jekylldev', 'css', 'watch'));
